a47d4cf0c2ccb0c8899ed7f58a0a3b694c709eaf
Jaylen Smith <kobe26945@icloud.com>
2024-05-04 19:18:38 -0400

    Initial Commit

---
+++ HolySwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HolySwiftUI
+//
+//  Created by Jaylen Smith on 5/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HolySwiftUI/HolySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolySwiftUIApp.swift
+//  HolySwiftUI
+//
+//  Created by Jaylen Smith on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HolySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
